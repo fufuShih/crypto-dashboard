862379f2c06d2129524460e440b4c8e18fcc420d
Felix Shih <zx8714245@gmail.com>
2025-05-17 09:53:07 +0800

    feat: modify component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@pixi/layout/react';
+import '@pixi/layout';
+import { LayoutContainer, Sprite } from '@pixi/layout/components';
+import React, { useRef } from 'react';
+import { Container } from 'pixi.js';
+import { extend, useApplication, Application } from '@pixi/react';
+import { BunnySprite } from './components/Bunny';
+
+extend({
+    Container,
+    LayoutContainer,
+    Sprite
+});
+
+const LayoutResizer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const layoutRef = useRef<Container>(null);
+    const { app } = useApplication();
+
+    app.renderer.on('resize', () => {
+        if (layoutRef.current) {
+            layoutRef.current.layout = {
+                width: app.screen.width,
+                height: app.screen.height,
+            };
+        }
+    });
+
+    return (
+        <pixiContainer ref={layoutRef} layout={{}}>
+            {children}
+        </pixiContainer>
+    );
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Application resizeTo={window} background={'#1C1C1D'}>
+        <LayoutResizer>
+            <layoutContainer
+                layout={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#2C2C2E' }}
+            >
+                <layoutContainer layout={{ width: '80%', height: '80%', backgroundColor: '#FF3B30' }}>
+                    <BunnySprite />
+                </layoutContainer>
+            </layoutContainer>
+        </LayoutResizer>
+    </Application>
   )
 }
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+import '@pixi/layout/react';
+
+declare module '@pixi/layout/react' {
+  export * from '@pixi/layout/react';
+}
