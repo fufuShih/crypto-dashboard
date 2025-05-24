65dd0b94d4d1115813d7db1e031878a946011506
Felix Shih <zx8714245@gmail.com>
2025-05-24 22:22:15 +0800

    feat: modify style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 import ChartContainer from "../components/ChartContainer";
 
 export function HomePage() {
-    return (
-        <section className="container mx-auto px-4">
-            <ChartContainer />
-        </section>
-    );
+  return (
+      <section className="mx-auto px-4 ">
+          <ChartContainer />
+      </section>
+  );
 }
