d561b29af3ef7327d213f1381c88dc165017ca95
Felix Shih <zx8714245@gmail.com>
2025-05-25 11:21:41 +0800

    feat: modify the layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,24 @@
 import ChartContainer from "../components/ChartContainer";
+import Portfolio from "../components/Portfolio";
+import Search from "../components/Search";
 
 export function HomePage() {
   return (
-      <section className="mx-auto px-4 ">
-          <ChartContainer />
-      </section>
+    <div className="min-h-screen bg-gradient-to-br from-[#667eea] to-[#764ba2] p-6">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex gap-6">
+          {/* Left side - Chart */}
+          <div className="flex-1">
+            <ChartContainer />
+          </div>
+
+          {/* Right side - Search and Portfolio */}
+          <div className="w-80 space-y-6">
+            <Search />
+            <Portfolio />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
